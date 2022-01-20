b13c88fd836aaad51549e19422fd4c8c87f11d61
kevin imaho <kevin.imaho@gmail.com>
2022-01-20 09:06:36 +0400

    version Statique

--- lib/main.dart
+++ lib/main.dart
@@ -30,11 +30,56 @@ class _HistState extends State<Hist> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Container(
-        child: Text(
-          'test',
-          style: TextStyle(color: Colors.white),
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            flex: 5,
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: Text(
+                  "Vous venez de crevez un pneu à St André. Vous n'avez pas de téléphone vous décidez de faire du stop."
+                  "Une ford fiesta rouge s'arrête et le conducteur vous demande si vous voulez qu'il vous dépanne.",
+                  style: TextStyle(color: Colors.white, fontSize: 25),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.green,
+                  primary: Colors.white,
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                  ),
+                ),
+                onPressed: () {},
+                child: const Text(
+                    "Vous lui remerciez et vous montez dans la voiture"),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(15.0),
+              child: TextButton(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue.shade400,
+                  primary: Colors.white,
+                  textStyle: const TextStyle(fontSize: 20),
+                ),
+                onPressed: () {},
+                child: const Text(
+                    "Vous lui demandez s'il n'est pas un meurtrier avant !"),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
