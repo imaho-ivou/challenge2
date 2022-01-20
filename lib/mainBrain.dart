40149a0295dd60997b7b4a15cf79df8c153d7c58
kevin imaho <kevin.imaho@gmail.com>
2022-01-20 13:58:07 +0400

    mise a jour des enonces et des choix

--- lib/mainBrain.dart
+++ lib/mainBrain.dart
@@ -2,12 +2,27 @@ import 'hist.dart';
 
 class MainBrain {
   List<Hist> histoire = [
-    Hist('histoire 0', 'choix 1', 'choix 2'),
-    Hist('histoire 1', 'choix 1', 'choix 2'),
-    Hist('histoire 2', 'choix 1', 'choix 2'),
-    Hist('histoire 3', 'choix 1', 'choix 2'),
-    Hist('histoire 4', 'choix 1', 'choix 2'),
-    Hist('histoire 5', 'choix 1', 'choix 2')
+    Hist(
+        "Vous venez de crevez un pneu à St André. Vous n'avez pas de téléphone vous décidez de faire du stop. Une ford fiesta rouge s'arrête et le conducteur vous demande si vous voulez qu'il vous dépanne.",
+        "Vous lui demandez s'il n'est pas un meurtrier avant !",
+        "Vous lui remerciez et vous montez dans la voiture"),
+    Hist(
+        "Il acquiesce de la tête sans faire attention à la question.",
+        "Attends, mais je sais comment changer un pneu voyons !",
+        "Au moins il est honnête. Vous montez ! "),
+    Hist(
+        "Lorsqu'il commence à conduire, il vous demande d'ouvrir la boite à gant. À l’intérieur, vous trouverez un couteau ensanglanté, deux doigts coupés et un CD de T-Matt.",
+        " C'est lui ou moi, je prends le couteau et je le poignarde.",
+        "J'adore T-Matt, je lui donne le CD."),
+    Hist(" Woaw ! Quelle évasion ! ", " Recommencer", ''),
+    Hist(
+        "En traversant la route du littoral, vous réfléchissez à la sagesse douteuse de poignarder quelqu’un pendant qu’il conduit une voiture dans laquelle vous êtes.",
+        "Recommencer",
+        ""),
+    Hist(
+        "Vous vous faites un bon dalon et vous chantez le dernier son de T-matt ensemble. Il vous dépose à Cambaie et il vous demande si vous connaissez un bon endroit pour jeter un corps.",
+        "Recommencer",
+        "")
   ];
 
   int compteur = 0;
